9cfb56c513b190d6cf27ca88b5cac3bb3c03ee85
kc8son <kc8son@gmail.com>
2023-06-30 12:06:53 -0500

    Added docker_down
    Added the scrip to sht down all docker containers.  This pipes 'docker ps' to awk to get all of the container id's.  Then it uses those container ids for the docker kill command.

--- Notes.bash
+++ Notes.bash
@@ -17,3 +17,23 @@
 #
 #   pinout
 #   https://pinout.xyz/
+#
+
+
+#   Docker_down
+#!/usr/bin/env bash
+####################################################################################################
+#
+#       Date Written: 11/10/2022        By: Joseph P. Merten
+#       Just a quick one liner to get the docker tags and shut them all down.
+#
+####################################################################################################
+#       Kill all the docker processes.
+docker kill $( docker ps | awk 'NR>1 { print $1}' )
+
+####################################################################################################
+#       remove the volume for MSSQL.
+#docker rm /sql2022
+#docker rm /Oracle001
+#docker rm /mongodb
+#docker rm /db
