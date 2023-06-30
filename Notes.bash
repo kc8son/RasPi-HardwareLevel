68560d53a5880e75ba54be3e7241bcb48c2e523d
Joseph P Merten <JoeMerten@RockCentral.com>
2023-06-30 09:12:54 -0400

    Just some quick notes

---
+++ Notes.bash
@@ -0,0 +1,19 @@
+####################################################################################################
+#
+#   Date Written: 06/30/2023        By: Joseph P. Merten
+#   This is just a file of notes about various hardware/system related commands.
+#
+#
+####################################################################################################
+#   neofetch:
+#   https://en.wikipedia.org/wiki/Neofetch
+#
+#   screenfetch:
+#   https://www.tecmint.com/screenfetch-system-information-generator-for-linux/
+#
+#   bpytop
+#   https://www.tecmint.com/bpytop-linux-resource-monitor-tool/
+#
+#
+#   pinout
+#   https://pinout.xyz/
